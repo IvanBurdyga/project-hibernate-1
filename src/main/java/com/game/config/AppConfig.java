1c5d23f5161de00bc4ccd6d1780677db425d8a51
Ivan <bukashivash@gmail.com>
2025-09-28 21:43:21 +0300

    ADD AppConfig FIX Player ADD PlayerRepositoryDB ADD Spy Driver

--- src/main/java/com/game/config/AppConfig.java
+++ src/main/java/com/game/config/AppConfig.java
@@ -3,8 +3,24 @@ package com.game.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.util.Properties;
+
 @Configuration
 @ComponentScan("com.game")
 public class AppConfig {
 
+    private static final Properties properties = new Properties();
+
+    static {
+        try {
+            properties.load(AppConfig.class.getResourceAsStream("/application.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getProperties(String key) {
+        return properties.getProperty(key);
+    }
 }
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,51 +1,143 @@
 package com.game.repository;
 
+import com.game.config.AppConfig;
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public static final String DB_DRIVER = "db.driver";
+    public static final String DB_URL = "db.uri";
+    public static final String DB_USER = "db.user";
+    public static final String DB_PASSWORD = "db.password";
+    public static final String DB_DIALECT = "db.dialect";
+
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, AppConfig.getProperties(DB_DRIVER));
+        properties.put(Environment.URL, AppConfig.getProperties(DB_URL));
+        properties.put(Environment.DIALECT, AppConfig.getProperties(DB_DIALECT));
+        properties.put(Environment.USER, AppConfig.getProperties(DB_USER));
+        properties.put(Environment.PASS, AppConfig.getProperties(DB_PASSWORD));
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Player.class)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        List<Player> players;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Query<Player> query = session.createNativeQuery("SELECT * FROM player", Player.class);
+            players = query
+                    .setFirstResult(pageNumber * pageSize)
+                    .setMaxResults(pageSize)
+                    .list();
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            session.close();
+        }
+        return players;
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+       int count;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Query<Long> query = session.createNamedQuery(Player.SELECT_COUNT_OF_PLAYER, Long.class);
+            count = Math.toIntExact(query.getSingleResult());
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            session.close();
+        }
+        return count;
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(player);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            session.close();
+        }
+        return player;
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(player);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        }
+        return player;
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            return Optional.ofNullable(session.get(Player.class, id));
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.remove(player);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException(e);
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
